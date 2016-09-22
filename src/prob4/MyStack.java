ca3271d647034ca10531c090160bfa2f424c359a
YJ <YJ@DESKTOP-S5IRBNE>
2016-09-22 19:37:17 +0900

    말하는 방식 고운말로 수정

--- src/prob4/MyStack.java
+++ src/prob4/MyStack.java
@@ -15,21 +15,21 @@ public class MyStack implements Stack {
 			// if (this.top != this.size()) {
 			this.array[top] = item;
 			this.top++;
-			System.out.println(item + "을 잘 넣었구욤, top은 " + this.top + "에용");
+			System.out.println("["+item + "]을 잘 넣었습니다, top의 번호는 [" + this.top + "]입니다");
 			// }
 		} catch (Exception e) {
-			System.out.println("===다찼어욤. 배열을 2배로 늘릴께염===");
+			System.out.println("===스택의 공간이 부족합니다. 공간을 2배 늘리겠습니다===");
 			String[] str = new String[(this.array.length) * 2];
 			System.arraycopy(this.array, 0, str, 0, this.array.length);
 			this.array = str;
 
 			this.array[top] = item;
 			this.top++;
-			System.out.println(item + "을 잘 넣었구욤, top은 " + this.top + "에용");
+			System.out.println("["+item + "]을 잘 넣었습니다, top의 번호는 [" + this.top + "]입니다");
 		}
 		System.out.println("===========배열 내용==============");
 		for (int i = 0; i < this.array.length; i++) {
-			System.out.println("배열내용 : 	" + array[i] + ", 	배열번호 : " + i);
+			System.out.println("배열내용 : 	[" + array[i] + "],    	배열번호 : [" + i+"]");
 		}
 		System.out.println("=============================");
 	}
@@ -40,7 +40,7 @@ public class MyStack implements Stack {
 			this.isEmpty();
 			throw new MyStackException(": stack is empty");
 		}
-		System.out.println(this.array[this.top] + "을 뺄거구욤, top은 " + this.top + "에용");
+		System.out.println("["+this.array[this.top] + "]을 빼내겠습니다, top의 번호는 [" + this.top + "]입니다");
 		for (int i = this.top; i > 0; i--) {
 			this.array[i - 1] = this.array[i];
 			this.array[i] = null;
@@ -50,10 +50,10 @@ public class MyStack implements Stack {
 		// this.top + "에용");
 		System.out.println("===========배열 내용==============");
 		for (int i = 0; i < this.array.length; i++) {
-			System.out.println("배열내용 : 	" + array[i] + ", 	배열번호 : " + i);
+			System.out.println("배열내용 : 	[" + array[i] + "], 	배열번호 : [" + i+"]");
 		}
 		System.out.println("=============================");
-		return "이제 top는 " + this.array[this.top] + "이구요, top은 " + this.top + "에용";
+		return "현재 top의 내용은[" + this.array[this.top] + "]입니다, top의 번호는 [" + this.top + "]입니다";
 	}
 
 	@Override
