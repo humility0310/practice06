67cd1346a9f3fd88385c945c013124e6c34792a0
yj <bit-user@bit>
2016-09-22 17:50:17 +0900

    전부완료

--- src/prob5/Money.java
+++ src/prob5/Money.java
@@ -11,16 +11,49 @@ public class Money {
 		this.amount = amount;
 	}
 
-	public Money add( Money money ) {
-		return null;
-    }
-    public Money minus( Money money ) {
-    	return null;
-    }
-    public Money multiply( Money money ) {
-    	return null;
-      }
-    public Money devide( Money money ) {
-    	return null;
-    }
+	public Money add(Money money) {
+		Integer result = this.amount + money.getAmount();
+		return new Money(result);
+	}
+
+	public Money minus(Money money) {
+		Integer result = this.amount - money.getAmount();
+		return new Money(result);
+	}
+
+	public Money multiply(Money money) {
+		Integer result = this.amount * money.getAmount();
+		return new Money(result);
+	}
+
+	public Money devide(Money money) {
+		Integer result = this.amount / money.getAmount();
+		return new Money(result);
+	}
+
+	public Money(int i) {
+		this.amount = i;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + amount;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Money other = (Money) obj;
+		if (amount != other.amount)
+			return false;
+		return true;
+	}
 }
